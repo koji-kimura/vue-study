d236289b3960652c671eaed5a7ff7523d52d915e
k-kimura <kimura_511@me.com>
2018-12-14 09:38:23 +0900

    アプリ完成

--- wonderful-quotes/src/App.vue
+++ wonderful-quotes/src/App.vue
@@ -1,12 +1,46 @@
 <template>
     <div class="container">
-    
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes ="quotes" @quoteDelete="quoteDelete"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Info:click on a quote to delete it!
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import  Header from "./components/Header.vue";
+    import  QuoteGrid from "./components/QuoteGrid.vue";
+    import  NewQuote from "./components/NewQuote.vue";
+
     export default {
-        
+        data: function(){
+            return {
+                quotes: ['Just a quote see something'],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            newQuote(quote){
+                if(this.quotes.length >= this.maxQuotes){
+                    return alert('please delete quote first')
+                }
+                this.quotes.push(quote);
+            },
+            quoteDelete(index){
+                this.quotes.splice(index,1);
+            }
+        },
+        components:{
+            appHeader: Header,
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
+        }
     }
 </script>
 
